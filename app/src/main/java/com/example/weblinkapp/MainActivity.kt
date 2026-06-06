e3b9c1222305e57caf5a98d965f320c11377765d
Langu Valymas <langu.valymas@users.noreply.github.com>
2026-06-06 13:46:58 +0000

    Pataisyta: pridėtas LayoutInflater import

--- app/src/main/java/com/example/weblinkapp/MainActivity.kt
+++ app/src/main/java/com/example/weblinkapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weblinkapp
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
